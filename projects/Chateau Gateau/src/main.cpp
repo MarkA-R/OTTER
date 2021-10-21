fafcf6f5eec1c0dd65c6638c441e42264b5347d2
Gohembo <gohembo@gmail.com>
2021-10-21 16:37:43 -0400

    Starting ECS

--- projects/Chateau Gateau/src/main.cpp
+++ projects/Chateau Gateau/src/main.cpp
@@ -1,1 +1,33 @@
-int main() { return 0; } 
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <filesystem>
+#include <json.hpp>
+#include <fstream>
+#include <entt.hpp>
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+#include <Camera.h>
+
+/*
+#include "IndexBuffer.h"
+#include "VertexBuffer.h"
+#include "VertexArrayObject.h"
+#include "Shader.h"
+#include "Camera.h"
+
+#include "Utils/ObjLoader.h"
+#include "VertexTypes.h"
+*/
+
+int main() { 
+	entt::registry registry;
+	entt::entity entity = registry.create();
+	registry.emplace<Camera>(entity);
+	
+	return 0;
+	
+} 
