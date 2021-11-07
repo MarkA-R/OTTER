8f5e13e460db7e12ff1f61eaa13d3b7e3f4b67f7
Gohembo <gohembo@gmail.com>
2021-11-07 14:06:18 -0500

    made bakeryUtils into one /h

--- projects/Chateau_Gateau/src/bakeryUtils.h
+++ projects/Chateau_Gateau/src/bakeryUtils.h
@@ -28,19 +28,56 @@ public:
         CREAM
     };
 
-    static float returnCroissantTime();
+    static float bakeryUtils::returnCroissantTime()
+    {
+        return timeToCroissant;
+    }
+    static float bakeryUtils::returnCookieTime()
+    {
+        return timeToCookie;
+    }
+    static float bakeryUtils::returnCupcakeTime()
+    {
+        return timeToCupcake;
+    }
+    static float bakeryUtils::returnCakeTime()
+    {
+        return timeToCake;
+    }
+    static float bakeryUtils::returnBurnTime()
+    {
+        return timeToBurn;
+    }
 
-    static float returnCookieTime();
-
-    static float returnCupcakeTime();
-
-    static float returnCakeTime();
-
-    static float returnBurnTime();
-
-    static float returnBakeTime(bakeryUtils::pastryType t);
-
-    bakeryUtils();
+    static float bakeryUtils::returnBakeTime(bakeryUtils::pastryType t)
+    {
+        if ((int)t == (int)bakeryUtils::pastryType::DOUGH)
+        {
+            return 0.f;
+        }
+        else if ((int)t == (int)bakeryUtils::pastryType::CROISSANT)
+        {
+            return returnCroissantTime();
+        }
+        else if ((int)t == (int)bakeryUtils::pastryType::COOKIE)
+        {
+            return returnCookieTime();
+        }
+        else if ((int)t == (int)bakeryUtils::pastryType::CUPCAKE)
+        {
+            return returnCupcakeTime();
+        }
+        else if ((int)t == (int)bakeryUtils::pastryType::CAKE)
+        {
+            return returnCakeTime();
+        }
+        else if ((int)t == (int)bakeryUtils::pastryType::BURNT)
+        {
+            return returnBurnTime();
+        }
+        return returnBurnTime();
+    }
+    
 
 protected:
     inline static float timeToCroissant = 10;
